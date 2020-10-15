a8b2c6c856a206a7a6a984bd62c00e9e4bda67b5
Nicolas Mariángel <nicolas.mariangel@usach.cl>
2020-10-15 18:33:03 -0300

    Move and rename educalingo.rb file, plus parse html response to read Educalingo's 'synonym' word cloud

---
+++ lib/synonym_scrapper/educalingo.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'synonym_scrapper/scrapper'
+
+class Educalingo < Scrapper
+
+	def initialize()
+		super(5, "https://educalingo.com/en/dic-es/")
+	end
+
+	def build_call_url(word)
+		URI.parse(base_url + word)
+	end
+
+	def synonyms(word)
+		response = call(word)
+		doc = Nokogiri.HTML(response)
+		synonyms = Array.new
+		doc.css('#wordcloud1 > span').each do |synonym|
+			score = Integer(synonym.values[0])
+
+			synonyms.push({
+				word: synonym.inner_html,
+				score: score
+			}) unless score < 75
+			# A minimum score of 75 is considered because educalingo
+			# tends to have completely unrelated words around this score
+		end
+
+		return synonyms
+	end
+end
