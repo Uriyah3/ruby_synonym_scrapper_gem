da51f8614187e8d08d2587ef7ee91a38150ff151
Nicolas Mariángel <nicolas.mariangel@usach.cl>
2020-10-15 18:35:36 -0300

    Scrape the synonyms of a word from the dictionary selected

--- lib/synonym_scrapper.rb
+++ lib/synonym_scrapper.rb
@@ -1,21 +1,24 @@
-class Hola
-  def self.hi(language = "english")
-    translator = Translator.new(language)
-    translator.hi
-  end
-end
+require 'synonym_scrapper/datamuse'
+require 'synonym_scrapper/educalingo'
+require 'synonym_scrapper/nltk'
 
-class Hola::Translator
-  def initialize(language)
-    @language = language
+class SynonymScrapper
+
+  # Define once all dictionaries that can be used
+  @@synonym_dictionaries = {
+    Datamuse: Datamuse.new,
+    Educalingo: Educalingo.new,
+    Nltk: Nltk.new
+  }
+
+  def synonyms(word, dictionary)
+    return "Dictionary does not exist" unless synonym_dictionaries.key?(dictionary)
+
+    return synonym_dictionaries[dictionary].synonyms(word)
   end
 
-  def hi
-    case @language
-    when "spanish"
-      "hola mundo"
-    else
-      "hello world"
-    end
+  def synonym_dictionaries()
+    @@synonym_dictionaries
   end
+
 end
