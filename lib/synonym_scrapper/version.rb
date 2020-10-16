ece5381d5953b6ef2866a43913e4eeddf2352b0e
Nicolas Mariángel <nicolas.mariangel@usach.cl>
2020-10-16 12:30:57 -0300

    Combine current files with bundler's boilerplate

---
+++ lib/synonym_scrapper/version.rb
@@ -0,0 +1,3 @@
+module SynonymScrapper
+  VERSION = "0.1.0"
+end
---
+++ test/synonym_scrapper_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class SynonymScrapperTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::SynonymScrapper::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
